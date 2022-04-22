eba8effb1ba27d83ef491b5c84b7fab35e91de2a
mijan-joy <mmrsm46@gmail.com>
2022-04-22 15:09:54 +0600

    now work for server site

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -45,6 +45,7 @@ const Login = () => {
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
         signInWithEmailAndPassword(email, password);
+       
     }
     const navigateRegister = (event) => {
         navigate('/register');
--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
+
+
 
 const RequireAuth = ({children}) => {
     const [user, loading] = useAuthState(auth);
     const location = useLocation();
+    const [ sendEmailVerifiaction, sending, error] = useSendEmailVerification(auth)
     if(loading){
       return <Loading></Loading>
     }
 
     if (!user) {
         return <Navigate to="/login" state={{ from: location }} replace />;
+      }
+      
+      if(user.providerData[0]?.providerId === 'password' && !user.emailVerified){
+        return <div className=' text-center mt-5'>
+          <h3 className='text-danger'>Email is not verified</h3>
+          <h5 className='text-success'>Please verify your email</h5>
+          <button className='btn btn-success' onClick={async () =>{
+            await sendEmailVerifiaction()
+            toast('Email is sent')
+          }}>Send Verificaiton Email Again</button>
+          <ToastContainer></ToastContainer>
+        </div>
       }
     
       return children;
